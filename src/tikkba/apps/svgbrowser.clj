5e3f7058981f8d9deaa21f526f8135ffc7da9384
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2011-04-11 15:44:43 +0200

    undo / redo functions based on javax.swing.undo.*

--- src/tikkba/apps/svgbrowser.clj
+++ src/tikkba/apps/svgbrowser.clj
@@ -6,113 +6,47 @@
   tikkba.apps.svgbrowser
   (:use clojure.pprint)
   (:require [tikkba.utils.dom :as dom])
-  (:import (org.apache.batik.apps.svgbrowser
-            HistoryBrowserInterface
-            HistoryBrowser
-            HistoryBrowser$CommandController)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; wrappers
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(defn history-browser-interface
-  "Constructor. 
-
-   See org.apache.batik.apps.svgbrowser.HistoryBrowserInterface"
-  [command-controller]
-  (HistoryBrowserInterface. command-controller))
+  (:import (javax.swing.undo AbstractUndoableEdit CompoundEdit)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; helper functions
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defprotocol Cmd
-  "A protocol representing a command to execute"
-  (execute [this])
-  (undo [this])
-  (redo [this]))
+(defmacro create-edit
+  "Creates an implementation of an AbstractUndoableEdit
+   and executes the todo function"
+  [todo toundo toredo]
+  `(let  [todo# ~todo
+          toundo# ~toundo
+          toredo# ~toredo]
+     (todo#)
+     (proxy [AbstractUndoableEdit] []
+       (undo
+        []
+        (toundo#)
+        (proxy-super undo))
 
-(defrecord TikkbaCmd
-    [toexecute toundo toredo]
-    Cmd
-    (execute
-     [this]
-     (toexecute))
-    
-    (undo
-     [this]
-     (toundo))
+       (redo
+        []
+        (toredo#)
+        (proxy-super redo)))))
 
-    (redo
-     [this]
-     (toredo)))
-
-(defprotocol CmdStack
-  "A stack of commands that can be undone or redone"
-  (add-cmd [this cmd])
-  (can-undo? [this])
-  (undo-cmd [this])
-  (redo-cmd [this])
-  (can-redo? [this]))
-
-(defrecord TikkbaCmdStack
-    [cmds idx]
-  CmdStack
-
-  (add-cmd
-   [this cmd]
-   (execute cmd)
-   (let [stack this
-         stack (update-in stack [:idx] inc)
-         stack (update-in stack [:cmds] subvec 0 (:idx stack))
-         stack (update-in stack [:cmds] conj cmd)]
-     stack))
-
-  (can-undo?
-   [this]
-   (not (neg? idx)))
-
-  (can-redo?
-   [this]
-   (not= idx (dec (count cmds))))
-
-  (undo-cmd
-   [this]
-   (if (can-undo? this)
-     (let [currentcmd (get cmds idx)]
-       (undo currentcmd)
-       (update-in this [:idx] dec))
-     this))
-
-  (redo-cmd
-   [this]
-   (if (can-redo? this)
-     (let [currentcmd (get cmds idx)]
-       (redo currentcmd)
-       (update-in this [:idx] inc))
-     this)))
-
-(defn cmdstack
-  "Creates a new simple command stack"
-  []
-  (TikkbaCmdStack. [] -1))
-
-(defn node-removed-cmd
-  "Creates a node removed command"
+(defn node-removed-edit
+  "Performs and reates a node removed edit"
   [oldparent oldsibling node]
-  (letfn [(toexecute
-           []
-           (dom/remove-child oldparent node))
+  (letfn [(todo
+            []
+            (dom/remove-child oldparent node))
 
           (toundo
            []
            (dom/insert-before oldparent node oldsibling))]
-    (TikkbaCmd. toexecute toundo toexecute)))
+    (create-edit todo toundo todo)))
 
-(defn node-inserted-cmd
-  "Creates a node inserted command"
+(defn node-inserted-edit
+  "Performs and creates a node inserted edit"
   [parent sibling node]
-  (letfn [(toexecute
+  (letfn [(todo
            []
            (if (nil? sibling)
              (dom/append-child parent node)
@@ -121,5 +55,14 @@
           (toundo
            []
            (dom/remove-child parent node))]
-    (TikkbaCmd. toexecute toundo toexecute)))
+    (create-edit todo toundo todo)))
+
+(defn compound-edit
+  "Creates a compound edit"
+  [& edits]
+  (let [compoundedit (CompoundEdit.)]
+    (doseq [edit edits]
+      (.addEdit compoundedit edit))
+    (.end compoundedit)
+    compoundedit))
 
