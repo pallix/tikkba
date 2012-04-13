cd67786751e24a367408bc82b547c373ae909cf4
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2012-04-13 11:12:03 +0200

    added test for issue #1

---
+++ test/tikkba/test/functional/svgattr.clj
@@ -0,0 +1,17 @@
+(ns tikkba.test.functional.svgattr
+  (:use [analemma svg xml]
+        [tikkba swing dom])
+  (:require [tikkba.utils.dom :as dom]))
+
+(defn create-svg
+  []
+  (svg {:width "15cm" :height "15cm" :viewBox "0 0 15cm 15cm"}
+        (-> (circle "8cm" "8cm" "6cm")
+            (style :stroke "#000000" :fill "none"))))
+
+(defn -main
+  []
+  (let [doc (svg-doc (create-svg))]
+   (dom/spit-xml "/tmp/svgattr.svg" doc
+                 :indent "yes"
+                 :encoding "UTF8")))
