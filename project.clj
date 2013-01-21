15cff08d1eb2e2084ffb458ea5b87d5abd33d8cb
Pierre Allix <pierre.allix@fokus.fraunhofer.de>
2013-01-21 09:50:31 +0100

    upgrade version to 0.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tikkba/tikkba "0.4.0" 
+(defproject tikkba/tikkba "0.4.1" 
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "[1.2.0,]"]
                  [org.clojars.pallix/analemma "1.0.0"]
