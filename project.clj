664e1d2b16f3d88520e951ba478db268fbc8083c
Pierre Allix <pierre.allix@liqd.de>
2016-04-02 19:36:06 +0200

    Add license and project URL in project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject tikkba/tikkba "0.6.0"
   :min-lein-version "2.0.0"
+  :url "https://github.com/pallix/lacij"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojars.pallix/analemma "1.0.0" :exclusions [org.clojure/clojure]]
                  [org.clojars.pallix/batik "1.7.0"]]
