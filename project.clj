588bd6ebc03cfa7ef388656de4de5399d5d2b025
Pierre Allix <pierre.allix@liqd.de>
2016-04-02 19:28:43 +0200

    Cleanup dependencies versions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tikkba/tikkba "0.5.0"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "[1.2.0,]"]
-                 [org.clojars.pallix/analemma "1.0.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojars.pallix/analemma "1.0.0" :exclusions [org.clojure/clojure]]
                  [org.clojars.pallix/batik "1.7.0"]]
   :description "Tikkba is Clojure library for the creation and the dynamic modification\nof SVG documents. It wraps the Apache Batik library and provides functions to\ncreate SVG images with the Clojure-based SVG DSL of the Analemma library.")
